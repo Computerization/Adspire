983eed47bd68ce009383b6f8d660efa20459c5e6
qwerzl <me@qwerzl.me>
2024-06-02 20:11:15 +0800

    chore: use Vercel Blob and Vercel Postgres

--- payload.config.ts
+++ payload.config.ts
@@ -1,16 +1,14 @@
 import { media, pages, sessions, users } from '@/payload/collections'
 import { COLLECTION_SLUG_MEDIA, COLLECTION_SLUG_PAGE, COLLECTION_SLUG_PRODUCTS } from '@/payload/collections/config'
 import { siteSettings } from '@/payload/globals/site-settings'
-import { S3_PLUGIN_CONFIG } from '@/payload/plugins/s3'
 import generateBreadcrumbsUrl from '@/payload/utils/generateBreadcrumbsUrl'
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { resendAdapter } from '@payloadcms/email-resend'
 import { formBuilderPlugin } from '@payloadcms/plugin-form-builder'
 import { seoPlugin } from '@payloadcms/plugin-seo'
 import { nestedDocsPlugin } from '@payloadcms/plugin-nested-docs'
 import { stripePlugin } from '@payloadcms/plugin-stripe'
 import { FixedToolbarFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
-import { s3Storage as s3StoragePlugin } from '@payloadcms/storage-s3'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 import { prices, products, subscriptions } from '@/payload/collections/stripe'
 import { priceUpsert, subscriptionDeleted, subscriptionUpsert } from '@/payload/stripe/webhooks'
@@ -20,6 +18,7 @@ import { en } from 'payload/i18n/en'
 import sharp from 'sharp'
 import { fileURLToPath } from 'url'
 import _get from 'lodash/get'
+import {postgresAdapter} from "@payloadcms/db-postgres";
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -39,8 +38,10 @@ export default buildConfig({
     features: ({ defaultFeatures }) => [...defaultFeatures, FixedToolbarFeature()]
   }),
   secret: process.env.AUTH_SECRET || '',
-  db: mongooseAdapter({
-    url: process.env.MONGODB_URI || ''
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.POSTGRES_URL,
+    },
   }),
   serverURL: process.env.NEXT_PUBLIC_SITE_URL,
   email:
@@ -60,17 +61,11 @@ export default buildConfig({
       generateURL: generateBreadcrumbsUrl,
       breadcrumbsFieldSlug: 'breadcrumbs'
     }),
-    s3StoragePlugin({
-      ...S3_PLUGIN_CONFIG,
+    vercelBlobStorage({
       collections: {
-        [COLLECTION_SLUG_MEDIA]: {
-          disableLocalStorage: true,
-          generateFileURL: (args: any) => {
-            return `https://${process.env.NEXT_PUBLIC_S3_HOSTNAME}/${args.prefix}/${args.filename}`
-          },
-          prefix: process.env.NEXT_PUBLIC_UPLOAD_PREFIX || 'media'
-        }
-      }
+        [media.slug]: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN || '',
     }),
     formBuilderPlugin({
       redirectRelationships: [COLLECTION_SLUG_PAGE],
